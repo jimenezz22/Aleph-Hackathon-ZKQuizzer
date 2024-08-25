a379f59df02a24dfc13707bcd8105ae2fa198e7c
jimenezz22 <ljimenezbogantes@gmail.com>
2024-08-25 04:22:00 -0300

    feat: add connection to metamask

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import './styles/global.css';
+import { MetaMaskButton } from "@metamask/sdk-react-ui"
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,9 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
         </Routes>
+      </div>
+      <div className="wallet">
+        <MetaMaskButton theme={"light"} color="white"></MetaMaskButton>
       </div>
     </Router>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetaMaskUIProvider } from "@metamask/sdk-react-ui"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskUIProvider
+      sdkOptions={{
+        dappMetadata: {
+          name: "Example React UI Dapp",
+          url: window.location.href,
+        },
+        infuraAPIKey: process.env.INFURA_API_KEY,
+      }}
+    >
+      <App />
+    </MetaMaskUIProvider>
   </React.StrictMode>
 );
 
